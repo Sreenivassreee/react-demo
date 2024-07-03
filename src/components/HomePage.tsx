f93ae8a1938ad068182d27ba74b6bcd933d3b3dd
Sreenivas K <k.sreenivasulu0143@gmail.com>
2024-07-03 19:53:31 +0530

    adding tailwindcss

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return    <h1 className="text-3xl font-bold underline bg-red">
+            Hello world!
+        </h1>
+        
+    
+
+
+}
+
+export default HomePage
